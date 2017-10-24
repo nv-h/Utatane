3bdbdd3e0e887c08717bf77e5bc6e12b1de211c6
nv-h <saido.nv@gmail.com>
2017-10-24 16:18:21 +0900

    微調整

--- README.md
+++ README.md
@@ -11,12 +11,9 @@
 Ricty生成スクリプトをフォークして生成したプログラミング用の等幅フォント[Cica](https://github.com/miiton/Cica)からさらにフォークしたフォントです。
 [Ubuntu Mono](http://font.ubuntu.com/) と
 [やさしさゴシック](http://www.fontna.com/blog/379/) と [やさしさゴシックボールド](http://www.fontna.com/blog/736/) を合成して少し調整しています。
-[NERDFonts](https://github.com/ryanoasis/nerd-fonts) を追加合成しています。
 
 ```
 o Yasashica
-|\
-* * NERDFonts (without Pomicons)
 |\
 * * Ubuntu Mono
  \
@@ -38,7 +35,6 @@ IPAフォントに置き換える場合 [IPAフォント](http://ossipedia.ipa.g
 * 英数字に Ubutnu Mono を使用しています
 * それ以外の文字に やさしさゴシック を使用しています
 * 非HiDPI（非Retina）のWindowsでも文字が欠けません
-* [Powelineパッチ](https://github.com/powerline/fontpatcher)適用済みです
 
 
 ## バリエーション
--- cica.py
+++ cica.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import fontforge
 import psMat
 import os
 import sys
 import math
 from logging import getLogger, StreamHandler, Formatter, DEBUG
+
 logger = getLogger(__name__)
 handler = StreamHandler()
 handler.setLevel(DEBUG)
@@ -14,15 +16,25 @@ handler.setFormatter(formatter)
 logger.setLevel(DEBUG)
 logger.addHandler(handler)
 
-# ASCENT = 850
-# DESCENT = 174
-ASCENT = 881
-DESCENT = 143
+# Ubuntu Mono
+# 800 x 200 = 1000(Em)
+# Win(Ascent, Descent)=(-170, -183)
+# hhea(height, width)=(-170, 183)
+
+# 日本語フォント
+# 905 x 119 = 1024(Em)
+# Win(Ascent, Descent)=(-24, 13)
+# hhea(height, width)=(-24, 168)
+
+WIDTH   = 1024
+ASCENT  = 905
+DESCENT = 200
+
 SOURCE = './sourceFonts'
 DIST = './dist'
 LICENSE = open('./LICENSE.txt').read()
 COPYRIGHT = open('./COPYRIGHT.txt').read()
-VERSION = '1.0.0'
+VERSION = '1.0.2'
 
 fonts = [
     {
@@ -35,44 +47,20 @@ fonts = [
          'ubuntu_mono': 'UbuntuMono-R.ttf',
          'japanese': '07YasashisaGothic-R.ttf',
          'ubuntu_weight_reduce': 0,
-         'japanese_weight_add': 0,
+         'japanese_weight_add': 5,
          'italic': False,
-    #  }, {
-    #      'family': 'Yasashica',
-    #      'name': 'Yasashica-RegularItalic',
-    #      'filename': 'Yasashica-RegularItalic.ttf',
-    #      'weight': 400,
-    #      'weight_name': 'Regular',
-    #      'style_name': 'Italic',
-    #      'ubuntu_mono': 'UbuntuMono-R.ttf',
-    #      'japanese': '07YasashisaGothic-R.ttf',
-    #      'ubuntu_weight_reduce': 0,
-    #      'japanese_weight_add': 0,
-    #      'italic': True,
     }, {
         'family': 'Yasashica',
         'name': 'Yasashica-Bold',
         'filename': 'Yasashica-Bold.ttf',
         'weight': 700,
         'weight_name': 'Bold',
-         'style_name': 'Bold',
+        'style_name': 'Bold',
         'ubuntu_mono': 'UbuntuMono-B.ttf',
         'japanese': '07YasashisaGothic-B.ttf',
         'ubuntu_weight_reduce': 0,
         'japanese_weight_add': 0,
         'italic': False,
-    # }, {
-    #     'family': 'Yasashica',
-    #     'name': 'Yasashica-BoldItalic',
-    #     'filename': 'Yasashica-BoldItalic.ttf',
-    #     'weight': 700,
-    #     'weight_name': 'Bold',
-    #     'style_name': 'Bold Italic',
-    #     'ubuntu_mono': 'UbuntuMono-B.ttf',
-    #     'japanese': '07YasashisaGothic-B.ttf',
-    #     'ubuntu_weight_reduce': 0,
-    #     'japanese_weight_add': 0,
-    #     'italic': True,
     }
 ]
 
@@ -98,14 +86,13 @@ def remove_glyph_from_ubuntu(_font):
 def check_files():
     err = 0
     for f in fonts:
-        if not os.path.isfile('./sourceFonts/%s' % f.get('ubuntu_mono')):
+        if not os.path.isfile(SOURCE + '/%s' % f.get('ubuntu_mono')):
             logger.error('%s not exists.' % f)
             err = 1
 
-        if not os.path.isfile('./sourceFonts/%s' % f.get('japanese')):
+        if not os.path.isfile(SOURCE + '/%s' % f.get('japanese')):
             logger.error('%s not exists.' % f)
             err = 1
-
 
     if err > 0:
         sys.exit(err)
@@ -117,11 +104,7 @@ def modify_jpfont():
     pass
 
 def set_os2_values(_font, _info):
-    weight = _info.get('weight')
-    style_name = _info.get('style_name')
-    _font.os2_weight = weight
-    _font.os2_width = 5
-    _font.os2_fstype = 0
+    # style_name = _info.get('style_name')
     # if style_name == 'Regular':
     #     _font.os2_stylemap = 64
     # elif style_name == 'Bold':
@@ -130,32 +113,36 @@ def set_os2_values(_font, _info):
     #     _font.os2_stylemap = 1
     # elif style_name == 'Bold Italic':
     #     _font.os2_stylemap = 33
-    _font.os2_vendor = 'TMNM'
+    weight = _info.get('weight')
+    _font.os2_weight = weight
+    _font.os2_width = 5
+    _font.os2_fstype = 0
+    _font.os2_vendor = 'HSAI'
     _font.os2_version = 1
     _font.os2_winascent = ASCENT
-    _font.os2_winascent_add = 0
+    _font.os2_winascent_add = False
     _font.os2_windescent = DESCENT
-    _font.os2_windescent_add = 0
-    _font.os2_typoascent = 693
-    _font.os2_typoascent_add = 0
-    _font.os2_typodescent = -141
-    _font.os2_typodescent_add = 0
-    _font.os2_typolinegap = 49
+    _font.os2_windescent_add = False
+    _font.os2_typoascent = ASCENT
+    _font.os2_typoascent_add = False
+    _font.os2_typodescent = -DESCENT
+    _font.os2_typodescent_add = False
+    _font.os2_typolinegap = 0
     _font.hhea_ascent = ASCENT
-    _font.hhea_ascent_add = 0
+    _font.hhea_ascent_add = False
     _font.hhea_descent = -DESCENT
-    _font.hhea_descent_add = 0
+    _font.hhea_descent_add = False
     _font.hhea_linegap = 0
-    _font.os2_panose = (2, 11, weight / 100, 9, 2, 2, 3, 2, 2, 7)
+    _font.os2_panose = (2, 11, weight/100, 9, 2, 2, 3, 2, 2, 7)
     return _font
 
 def align_to_center(_g):
     width = 0
 
     if _g.width > 700:
-        width = 1024
+        width = WIDTH
     else:
-        width = 512
+        width = WIDTH/2
 
     _g.width = width
     _g.left_side_bearing = _g.right_side_bearing = (_g.left_side_bearing + _g.right_side_bearing)/2
@@ -163,65 +150,113 @@ def align_to_center(_g):
 
     return _g
 
+def align_to_left(_g):
+    width = _g.width
+    _g.left_side_bearing = 0
+    _g.width = width
+
+def align_to_right(_g):
+    width = _g.width
+    bb = _g.boundingBox()
+    left = width - (bb[2] - bb[0])
+    _g.left_side_bearing = left
+    _g.width = width
+
+
 def modify_nerd(_g):
     align_left = [
         0xe0b0, 0xe0b1, 0xe0b4, 0xe0b5, 0xe0b8, 0xe0b9, 0xe0bc,
         0xe0bd, 0xe0c0, 0xe0c1, 0xe0c4, 0xe0c6, 0xe0c8, 0xe0cc, 0xe0cd,
-        0xe0ce, 0xe0d1, 0xe0d2,
+        0xe0d1, 0xe0d2,
     ]
     align_right = [
         0xe0b2, 0xe0b3, 0xe0b6, 0xe0b7, 0xe0b7, 0xe0ba, 0xe0bb, 0xe0be,
-        0xe0bf, 0xe0c2, 0xe0c3, 0xe0c5, 0xe0c7, 0xe0ca, 0xe0d4,
+        0xe0bf, 0xe0c2, 0xe0c3, 0xe0c5, 0xe0c7, 0xe0ca, 0xe0ce, 0xe0d4,
     ]
+    # Powerline
     if _g.encoding >= 0xe0b0 and _g.encoding <= 0xe0d4:
         _g.transform(psMat.translate(0, -55))
-        if _g.encoding >= 0xe0b8:
-            _g.width = 1024
-        else:
-            _g.width = 512
-        if _g.encoding >= 0xe0b8 and _g.encoding <= 0xe0bf:
-            _g.transform(psMat.scale(0.8, 1.0))
+        _g.width = WIDTH
+
+        if _g.encoding >= 0xe0b0 and _g.encoding <= 0xe0b7:
+            _g.transform(psMat.compose(psMat.scale(1.0, 0.982), psMat.translate(0, -1)))
             if _g.encoding in align_right:
                 bb = _g.boundingBox()
-                left = 1024 - (bb[2] - bb[0])
+                left = WIDTH - (bb[2] - bb[0])
                 _g.left_side_bearing = left
-                _g.width = 1024
+                _g.width = WIDTH
             if _g.encoding in align_left:
                 _g.left_side_bearing = 0
-                _g.width = 1024
+                _g.width = WIDTH
+
+        if _g.encoding >= 0xe0b8 and _g.encoding <= 0xe0bf:
+            _g.transform(psMat.compose(psMat.scale(0.8, 0.982), psMat.translate(0, -1)))
+            if _g.encoding in align_right:
+                bb = _g.boundingBox()
+                left = WIDTH - (bb[2] - bb[0])
+                _g.left_side_bearing = left
+                _g.width = WIDTH
+            if _g.encoding in align_left:
+                _g.left_side_bearing = 0
+                _g.width = WIDTH
 
         if _g.encoding >= 0xe0c0 and _g.encoding <= 0xe0c3:
             _g.transform(psMat.scale(0.7, 1.0))
             if _g.encoding in align_right:
                 bb = _g.boundingBox()
-                left = 1024 - (bb[2] - bb[0])
+                left = WIDTH - (bb[2] - bb[0])
                 _g.left_side_bearing = left
-                _g.width = 1024
+                _g.width = WIDTH
             if _g.encoding in align_left:
                 _g.left_side_bearing = 0
-                _g.width = 1024
+                _g.width = WIDTH
         if _g.encoding >= 0xe0c4 and _g.encoding <= 0xe0c7:
             if _g.encoding in align_right:
                 bb = _g.boundingBox()
-                left = 1024 - (bb[2] - bb[0])
+                left = WIDTH - (bb[2] - bb[0])
                 _g.left_side_bearing = left
-                _g.width = 1024
+                _g.width = WIDTH
             if _g.encoding in align_left:
                 _g.left_side_bearing = 0
-                _g.width = 1024
+                _g.width = WIDTH
         if _g.encoding == 0xe0c8 or _g.encoding == 0xe0ca:
             _g.transform(psMat.scale(0.7, 1.0))
             if _g.encoding in align_right:
                 bb = _g.boundingBox()
-                left = 1024 - (bb[2] - bb[0])
+                left = WIDTH - (bb[2] - bb[0])
                 _g.left_side_bearing = left
-                _g.width = 1024
+                _g.width = WIDTH
             if _g.encoding in align_left:
                 _g.left_side_bearing = 0
-                _g.width = 1024
+                _g.width = WIDTH
+        if _g.encoding == 0xe0ce:
+            _g.transform(psMat.scale(0.8, 1.0))
+            bb = _g.boundingBox()
+            left = WIDTH - (bb[2] - bb[0])
+            _g.left_side_bearing = left
+            _g.width = WIDTH
+        if _g.encoding == 0xe0cf:
+            _g.transform(psMat.scale(0.9, 1.0))
+            _g = align_to_center(_g)
+        if _g.encoding == 0xe0d0:
+            _g = align_to_center(_g)
+        if _g.encoding == 0xe0d1:
+            _g.transform(psMat.compose(psMat.scale(1.0, 0.982), psMat.translate(0, -1)))
+            _g.left_side_bearing = 0
+            _g.width = WIDTH
+        if _g.encoding == 0xe0d2 or _g.encoding == 0xe0d4:
+            _g.transform(psMat.compose(psMat.scale(1.0, 0.982), psMat.translate(0, -1)))
+            if _g.encoding in align_right:
+                bb = _g.boundingBox()
+                left = WIDTH - (bb[2] - bb[0])
+                _g.left_side_bearing = left
+                _g.width = WIDTH
+            if _g.encoding in align_left:
+                _g.left_side_bearing = 0
+                _g.width = WIDTH
     else:
         _g.transform(psMat.translate(0, -55))
-        _g.width = 1024
+        _g.width = WIDTH
         _g = align_to_center(_g)
 
     return _g
@@ -273,17 +308,38 @@ def add_smalltriangle(_f):
 
     for g in _f.glyphs():
         if g.encoding == 0x25be or g.encoding == 0x25b8:
-            g.width = 512
+            g.width = WIDTH/2
             g = align_to_center(g)
 
     return _f
 
+def fix_box_drawings(_f):
+    left = [
+        0x2510, 0x2518, 0x2524, 0x2555, 0x2556, 0x2557, 0x255b, 0x255c, 0x255d,
+        0x2561, 0x2562, 0x2563,
+    ]
+    right = [
+        0x250c, 0x2514, 0x251c, 0x2552, 0x2553, 0x2554, 0x2558, 0x2559, 0x255a,
+        0x255e, 0x255f, 0x2560,
+    ]
+
+    for g in _f.glyphs():
+        if g.encoding < 0x2500 or g.encoding > 0x256c:
+            continue
+        if g.encoding in left:
+            align_to_left(g)
+        elif g.encoding in right:
+            align_to_right(g)
+
+    return _f
+
+
 def build_font(_f):
     log('Generating %s ...' % _f.get('weight_name'))
-    ubuntu = fontforge.open('./sourceFonts/%s' % _f.get('ubuntu_mono'))
+    ubuntu = fontforge.open(SOURCE + '/%s' % _f.get('ubuntu_mono'))
     ubuntu = remove_glyph_from_ubuntu(ubuntu)
-    cica = fontforge.open('./sourceFonts/%s' % _f.get('japanese'))
-    nerd = fontforge.open('./sourceFonts/nerd.ttf')
+    cica = fontforge.open(SOURCE + '/%s' % _f.get('japanese'))
+    # nerd = fontforge.open(SOURCE + '/nerd.ttf')
 
     for g in ubuntu.glyphs():
         if _f.get('ubuntu_weight_reduce') != 0:
@@ -322,26 +378,29 @@ def build_font(_f):
         if  g.isWorthOutputting:
             if _f.get('italic'):
                 g.transform(psMat.skew(0.25))
+            if g.encoding >= 0x2500 and g.encoding <= 0x25af:
+                g.transform(psMat.compose(psMat.scale(1.024, 1.024), psMat.translate(0, -30)))
+                g = align_to_center(g)
             ubuntu.selection.select(g.encoding)
             ubuntu.copy()
             cica.selection.select(g.encoding)
             cica.paste()
 
-    for g in nerd.glyphs():
-        if g.encoding < 0xe0a0 or g.encoding > 0xf4ff:
-            continue
-        g = modify_nerd(g)
-        nerd.selection.select(g.encoding)
-        nerd.copy()
-        cica.selection.select(g.encoding)
-        cica.paste()
+    # for g in nerd.glyphs():
+    #     if g.encoding < 0xe0a0 or g.encoding > 0xf4ff:
+    #         continue
+    #     g = modify_nerd(g)
+    #     nerd.selection.select(g.encoding)
+    #     nerd.copy()
+    #     cica.selection.select(g.encoding)
+    #     cica.paste()
 
+    cica = fix_box_drawings(cica)
     cica = zenkaku_space(cica)
     cica = vertical_line_to_broken_bar(cica)
     cica = emdash_to_broken_dash(cica)
+    # cica = add_notoemoji(cica)
     cica = add_smalltriangle(cica)
-    # cica = add_powerline(cica)
-
 
     cica.ascent = ASCENT
     cica.descent = DESCENT
@@ -387,16 +446,29 @@ def build_font(_f):
     # cica.appendSFNTName(0x409,15, "")
     cica.appendSFNTName(0x409,16, _f.get('family'))
     cica.appendSFNTName(0x409,17, _f.get('style_name'))
-    fontpath = './dist/%s' % _f.get('filename')
+    fontpath = DIST + '/%s' % _f.get('filename')
     cica.generate(fontpath)
 
     cica.close()
     ubuntu.close()
-    nerd.close()
+    # nerd.close()
 
+
+def add_notoemoji(_f):
+    notoemoji = fontforge.open(SOURCE + '/NotoEmoji-Regular.ttf')
+    for g in notoemoji.glyphs():
+        if g.isWorthOutputting and g.encoding > 0x04f9:
+            g.transform((0.42,0,0,0.42,0,0))
+            g = align_to_center(g)
+            notoemoji.selection.select(g.encoding)
+            notoemoji.copy()
+            _f.selection.select(g.encoding)
+            _f.paste()
+    notoemoji.close()
+    return _f
 
 def add_devicons(_f):
-    devicon = fontforge.open('./sourceFonts/devicon.ttf')
+    devicon = fontforge.open(SOURCE + '/devicon.ttf')
     current = 0xE160
     for g in devicon.glyphs():
         if g.isWorthOutputting:
@@ -408,7 +480,7 @@ def add_devicons(_f):
             _f.paste()
             current = current + 1
     devicon.close()
-    gopher = fontforge.open('./sourceFonts/gopher.sfd')
+    gopher = fontforge.open(SOURCE + '/gopher.sfd')
     for g in gopher.glyphs():
         if g.isWorthOutputting:
             gopher.selection.select(0x40)
@@ -421,25 +493,6 @@ def add_devicons(_f):
             _f.paste()
     gopher.close()
     return _f
-
-def add_powerline(_f):
-    powerline = fontforge.open('./fontpatcher/fonts/powerline-symbols.sfd')
-    for g in powerline.glyphs():
-        if g.isWorthOutputting:
-            scale = psMat.scale(0.4, 0.4)
-            translate = psMat.translate(0, 0)
-            if g.unicode >= 0xe0b2:
-                translate = psMat.translate(88, 0)
-            matrix = psMat.compose(scale, translate)
-            g.transform(matrix)
-            g.width = 512
-            powerline.selection.select(g.encoding)
-            powerline.copy()
-            _f.selection.select(int(hex(g.unicode), 0))
-            _f.paste()
-    powerline.close()
-    return _f
-
 
 def main():
     print('')
